0604ba9f64e20ad5c7e8237df86bbf1c29296709
Dan Vanderkam <danvdk@gmail.com>
2015-02-17 15:58:22 -0500

    more thorough tests for readableview

--- test/readableview-test.ts
+++ test/readableview-test.ts
@@ -6,19 +6,6 @@ var expect = chai.expect;
 
 import ReadableView = require('../src/readableview');
 
-describe('User Model Unit Tests:', () => {
-
-    describe('2 + 4', () => {
-        it('should be 6', () => {
-            expect(2+4).to.equals(6);
-        });
-
-        it('should not be 7', () => {
-            expect(2+4).to.not.equals(7);
-        });
-    });
-});
-
 describe('ReadableView', function() {
   it('should read 8-bit unsigned ints', function() {
     var u8 = new Uint8Array(5);
@@ -40,5 +27,41 @@ describe('ReadableView', function() {
     expect(bytes.readUint8()).to.equal(127);
     expect(bytes.bytesRemaining()).to.equal(0);
     expect(bytes.tell()).to.equal(5);
+  });
+
+  it('should read strings', function() {
+    var u8 = new Uint8Array(6);
+    u8[0] = 4;
+    u8[1] = '2'.charCodeAt(0);
+    u8[2] = 'b'.charCodeAt(0);
+    u8[3] = 'i'.charCodeAt(0);
+    u8[4] = 't'.charCodeAt(0);
+    u8[5] = '?'.charCodeAt(0);
+
+    var bytes = new ReadableView(new DataView(u8.buffer));
+    expect(bytes.tell()).to.equal(0);
+    expect(bytes.bytesRemaining()).to.equal(6);
+    expect(bytes.readUint8()).to.equal(4);
+    expect(bytes.readAscii(4)).to.equal('2bit');
+    expect(bytes.tell()).to.equal(5);
+    expect(bytes.bytesRemaining()).to.equal(1);
+    expect(bytes.readAscii(1)).to.equal('?');
+    expect(bytes.bytesRemaining()).to.equal(0);
+  });
+
+  it('should read uint32 arrays', function() {
+    var u32 = new Uint32Array(5);
+    u32[0] = 1;
+    u32[1] = 2;
+    u32[2] = 12345678;
+    u32[3] = 1234567890;
+    u32[4] = 3;
+
+    var bytes = new ReadableView(new DataView(u32.buffer));
+    expect(bytes.tell()).to.equal(0);
+    expect(bytes.bytesRemaining()).to.equal(20);
+    expect(bytes.readUint32Array(5)).to.deep.equal([1,2,12345678,1234567890,3]);
+    expect(bytes.tell()).to.equal(20);
+    expect(bytes.bytesRemaining()).to.equal(0);
   });
 });
