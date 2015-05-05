c0c48a07d4760697f1809a217ce43ef283ed7978
Dan Vanderkam <danvdk@gmail.com>
2015-05-05 17:27:22 -0400

    Port over PileupTrack

--- src/Root.js
+++ src/Root.js
@@ -7,7 +7,6 @@
 import type * as SamRead from './SamRead';
 
 var React = require('react'),
-    ContigInterval = require('./ContigInterval'),
     Controls = require('./Controls'),
     GenomeTrack = require('./GenomeTrack'),
     GeneTrack = require('./GeneTrack'),
@@ -25,10 +24,7 @@ var Root = React.createClass({
   getInitialState: function() {
     return {
       contigList: ([]: string[]),
-      range: (null: ?GenomeRange),
-      basePairs: (null: any),
-      genes: ([]: Array<any>),  // TODO import Gene type
-      reads: ([]: SamRead[])
+      range: (null: ?GenomeRange)
     };
   },
   componentDidMount: function() {
@@ -36,38 +32,21 @@ var Root = React.createClass({
     var referenceSource = this.props.referenceSource;
     referenceSource.needContigs();
 
-    referenceSource.on('contigs', () => { this.update() });
+    referenceSource.on('contigs', () => {
+      this.setState({
+        contigList: referenceSource.contigList(),
+      });
+    });
 
     referenceSource.on('contigs', () => {
       // this is here to facilitate faster iteration
       this.handleRangeChange(this.props.initialRange);
     });
-
-    // var geneSource = this.props.geneSource;
-    // geneSource.on('newdata', () => { this.update() });
-
-    var bamSource = this.props.bamSource;
-    bamSource.on('newdata', () => { this.update() });
-
-    this.update();
-  },
-  update: function() {
-    var range = this.state.range,
-        ci = range && new ContigInterval(range.contig, range.start, range.stop);
-    this.setState({
-      contigList: this.props.referenceSource.contigList(),
-      // basePairs: this.props.referenceSource.getRange(range),
-      // genes: this.props.geneSource.getGenesInRange(ci),
-      reads: this.props.bamSource.getAlignmentsInRange(ci)
-    });
   },
   handleRangeChange: function(newRange: GenomeRange) {
     this.setState({range: newRange});
-    this.update();
 
-    var ref = this.props.referenceSource;
-    ref.rangeChanged(newRange);
-
+    this.props.referenceSource.rangeChanged(newRange);
     this.props.geneSource.rangeChanged(newRange);
     this.props.bamSource.rangeChanged(newRange);
     this.props.variantSource.rangeChanged(newRange);
@@ -89,7 +68,7 @@ var Root = React.createClass({
                    referenceSource={this.props.referenceSource}
                    onRangeChange={this.handleRangeChange} />
         <PileupTrack range={this.state.range}
-                     reads={this.state.reads}
+                     source={this.props.bamSource}
                      referenceSource={this.props.referenceSource}
                      onRangeChange={this.handleRangeChange} />
 
