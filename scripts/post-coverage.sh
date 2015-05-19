3e064f8fdb6f6f9b88f6f89104d463e22a690cfb
Dan Vanderkam <danvdk@gmail.com>
2015-05-19 15:27:20 -0400

    dump lcov to stdout

--- scripts/post-coverage.sh
+++ scripts/post-coverage.sh
@@ -5,6 +5,12 @@ set -x
 # Generate LCOV data for the bundled tests
 grunt coverage
 
+
+# dump it out temporarily for debugging
+./scripts/transform-coverage.js \
+  build/tests.map \
+  build/bundled.lcov
+
 # Convert code coverage data on the bundled test file back to the originals.
 ./scripts/transform-coverage.js \
   build/tests.map \
