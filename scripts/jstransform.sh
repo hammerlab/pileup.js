9faf23d71f303ce9f304073b2ae57674fa16f7c5
B. Arman Aksoy <arman@aksoy.org>
2015-06-29 16:42:50 -0400

    make jstransform.sh script more flexible

--- scripts/jstransform.sh
+++ scripts/jstransform.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-export JSTRANSFORM_OPTS="${JSTRANSFORM_OPTS} --react --harmony --strip-types --non-strict-es6module --source-map-inline"
-echo "$JSTRANSFORM_OPTS"
 # Transform all the code from src to dist
-jstransform $JSTRANSFORM_OPTS src dist/
+
+jstransform --react --harmony --strip-types \
+  --non-strict-es6module --source-map-inline \
+  $@ \
+  src/ dist/
