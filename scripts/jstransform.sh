4c611c212cd92d8f7415ce0f6703c580c033f5cb
B. Arman Aksoy <arman@aksoy.org>
2015-06-30 16:43:07 -0400

    fix cosmetic issues within jstransform script

--- scripts/jstransform.sh
+++ scripts/jstransform.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Transform all the code from src to dist
 
-jstransform --react --harmony --strip-types \
-  --non-strict-es6module --source-map-inline \
-  $@ \
-  src/ dist/
+jstransform \
+  --react \
+  --harmony \
+  --strip-types \
+  --non-strict-es6module \
+  --source-map-inline \
+  "$@" \
+  src/ \
+  dist/
