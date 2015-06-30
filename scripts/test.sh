6aa4729368d8d1977e92ddea744ea849ebe8fb75
B. Arman Aksoy <arman@aksoy.org>
2015-06-30 13:13:27 -0400

    ditch grunt: run tests via bash scripts

---
+++ scripts/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Starts the http-server and runs mocha-phantomjs-based tests
+
+# Run http-server and save its PID
+npm run http-server > /dev/null &
+SERVER_PID=$!
+sleep 1 # let the server settle down
+
+# Start the tests
+npm run mocha-phantomjs
+TEST_STATUS=$?
+
+# Kill the http server
+pkill -TERM -P $SERVER_PID
+
+# Exit with test status
+exit $TEST_STATUS
