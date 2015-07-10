5689ad52812a18a7bda3847a3e0e6259a7e10389
B. Arman Aksoy <arman@aksoy.org>
2015-07-10 14:39:46 -0400

    add a minimalistic axis to show the actual coverage measures

--- src/main/CoverageTrack.js
+++ src/main/CoverageTrack.js
@@ -1,5 +1,5 @@
 /**
- * Pileup visualization of BAM sources.
+ * Coverage visualization of BAM sources.
  * @flow
  */
 'use strict';
@@ -46,6 +46,8 @@ class NonEmptyCoverageTrack extends React.Component {
   }
 
   render(): any {
+    // Fill the container up
+    // as we are going to use almost all the space available
     var containerStyles = {
       'height': '100%'
     };
@@ -80,7 +82,13 @@ class NonEmptyCoverageTrack extends React.Component {
     this.updateSize();
 
     var div = this.refs.container.getDOMNode();
-    d3.select(div).append('svg');
+    var svg = d3.select(div).append('svg');
+
+    // Below, we create the container group for our bars upfront
+    // as we want to overlay the axis on top of them
+    // therefore we have to make sure their container
+    // is defined first in the SVG
+    svg.append('g').attr('class', 'bin-group');
 
     this.props.source.on('newdata', () => {
       var range = this.props.range,
@@ -109,42 +117,85 @@ class NonEmptyCoverageTrack extends React.Component {
     // Hold off until height & width are known.
     if (width === 0) return;
 
-    svg
-      .attr('width', width)
-      .attr('height', height);
+    svg.attr('width', width).attr('height', height);
 
+    // Calculate summary statistics for coverage from the reads
     var binCounts = _.chain(this.state.reads)
+      // Extract the interval a read covers
       .map(read => read.getInterval().interval)
+      // Create arrays for nucleotides covered by a read
       .map(interval => _.range(interval.start, interval.stop+1))
+      // Merge all covered reads into a single array
       .flatten()
+      // Group observations by nucleotide position and count them
       .countBy()
+      // Convert the pos=>count object into an array for easier parsing
       .pairs()
+      // Convert the array into a custom object to be used for D3 plotting
       .map(c => ({key: c[0], count: c[1]}))
+      // Extract the value from the underscore chain
       .value();
 
-    var maxVal = _.chain(binCounts).map(b => b.count).max().value();
+    var maxCoverage = _.chain(binCounts)
+      .map(b => b.count)
+      .max()
+      .value();
+    // Now that we know the max coverage,
+    // we now want to create a visually appealing axis
+    // to make it easy to comprehend for us, humans
+    // Rule: if maxCoverage is smaller than 10X,
+    //  then show it as it is (we like small numbers);
+    //  otherwise, round the number to the nearest tenth
+    //  (we hate numbers like 61)
+    maxCoverage = maxCoverage < 10
+      ? maxCoverage
+      : Math.round(maxCoverage/10)*10;
+
+    // We are also going to add some padding in two ways:
+    //   1. leave some padding at the bottom for axis origin text (axisHeight)
+    //   2. do not create an axis with the range = maxCoverage (axisMax)
+    var axisMax = maxCoverage * 1.2;  // +20% of the original value
+    var axisHeight = height * .9;  // use 90% of the available space
     var yScale = d3.scale.linear()
-      .domain([0, maxVal])
-      .range([0, height]);
+      .domain([axisMax, 0])  // mind the inverted axis
+      .range([0, axisHeight]);
 
-    var histBars = svg.selectAll("rect.covbin").data(binCounts, d => d.key);
+    // Select the group we created first
+    var histBars = svg.select('g.bin-group').selectAll("rect.covbin")
+      .data(binCounts, d => d.key);
 
+    // D3 logic for our histogram bars
     histBars
       .enter()
       .append('rect')
       .attr('x', d => xScale(d.key))
-      .attr('y', d => yScale(maxVal-d.count))
+      .attr('y', d => yScale(d.count))
       .attr('width', d => xScale(d.key) - xScale(d.key-1))
-      .attr('height', d => yScale(d.count))
+      .attr('height', d => yScale(axisMax-d.count))
       .attr('class', 'covbin');
-
     histBars
       .attr('x', d => xScale(d.key))
-      .attr('y', d => yScale(maxVal-d.count))
+      .attr('y', d => yScale(d.count))
       .attr('width', d => xScale(d.key) - xScale(d.key-1))
-      .attr('height', d => yScale(d.count))
-
+      .attr('height', d => yScale(axisMax-d.count))
     histBars.exit().remove();
+
+    // Logic for our axis
+    var yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient('left')  // this is gonna be at the far right
+      .tickSize(5)  // Make our ticks much more visible
+      .outerTickSize(0)  // Remove the default range ticks (they are ugly)
+      .tickFormat(t => t + 'X')  // X -> times in coverage terminology
+      .tickValues([0, maxCoverage/2, maxCoverage]);  // show min, avg and max
+    var yAxisEl = svg.selectAll('g.y-axis');
+    if(yAxisEl.empty()) {  // no axis element yet
+      svg.append('g')
+        .attr('class', 'y-axis')
+        .attr('transform', 'translate(' + width + ', 0)');
+    }
+    yAxisEl.call(yAxis);  // update the axis
+
   }
 }
 
