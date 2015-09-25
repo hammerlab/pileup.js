5a07209a066567f61c7393ca667fde8db333f57a
B. Arman Aksoy <arman@aksoy.org>
2015-09-25 13:05:40 -0400

    clean up the code and better test for labels

--- src/main/style.js
+++ src/main/style.js
@@ -17,8 +17,8 @@ module.exports = {
   },
 
   // Styles for base pairs which are rendered as letters
-  LOOSE_TEXT_STYLE: '24px Helvetica Neue, Helvetica, Arial, sans-serif',
-  TIGHT_TEXT_STYLE: 'bold 12px Helvetica Neue, Helvetica, Arial, sans-serif',
+  LOOSE_TEXT_STYLE: `24px 'Helvetica Neue', Helvetica, Arial, sans-serif`,
+  TIGHT_TEXT_STYLE: `bold 12px 'Helvetica Neue', Helvetica, Arial, sans-serif`,
 
   // Gene track
   GENE_ARROW_SIZE:4,
@@ -34,8 +34,7 @@ module.exports = {
   INSERT_COLOR: 'rgb(97, 0, 216)',
 
   // Coverage track
-  COVERAGE_FONT_SIZE: 9,  // match this number with the one in _STYLE
-  COVERAGE_FONT_STYLE: `bold 9px Helvetica Neue, Helvetica, Arial, sans-serif`,
+  COVERAGE_FONT_STYLE: `bold 9px 'Helvetica Neue', Helvetica, Arial, sans-serif`,
   COVERAGE_FONT_COLOR: 'black',
   COVERAGE_TICK_LENGTH: 5,
   COVERAGE_TEXT_PADDING: 3,  // space between axis ticks and text
