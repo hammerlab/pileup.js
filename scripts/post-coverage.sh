1fc2d94780320b3c4c979a9c7e43e2fc523a4d50
Dan Vanderkam <danvdk@gmail.com>
2015-10-22 18:07:11 -0400

    Track changes in the size of dist/pileup.min.js on Travis-CI.

--- scripts/post-coverage.sh
+++ scripts/post-coverage.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cat coverage/lcov.info | ./node_modules/.bin/coveralls
 
+# size=$(wc -c < dist/pileup.min.js | sed 's/ //g')
+# echo "Code size: $size"
+# curl --header "Authorization: token $GITHUB_TOKEN" --data '{"state": "success", "description": "'$size' bytes", "context": "Minified Code Size" }' https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$TRAVIS_COMMIT
+
+./scripts/post_code_size.py dist/pileup.min.js
+
 echo ''  # reset exit code -- failure to post coverage shouldn't be an error.
