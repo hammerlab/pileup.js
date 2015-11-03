3a495fe71f0ce8f72c7ad7f59fbe3280f7a25faa
Dan Vanderkam <danvdk@gmail.com>
2015-11-03 14:32:16 -0500

    Pull in travis-weigh-in from NPM

--- scripts/post-code-size.sh
+++ scripts/post-code-size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -o errexit
-curl -O https://raw.githubusercontent.com/danvk/travis-weigh-in/v1.0/weigh_in.py
 
-python weigh_in.py dist/pileup.min.js
+python node_modules/travis-weigh-in/weigh_in.py dist/pileup.min.js
+
