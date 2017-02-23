96dd40d8e5fce638087c0a42a0d8c65cab6a130f
Chris Gibb <chris.gibb@outlook.com>
2017-02-23 09:38:33 -0500

    Revert test aborting

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Starts the http-server and runs mocha-phantomjs-based tests
 # Note that you must run `npm run build` or `npm run watch` before running this.
-exit 0
 set -o errexit
 
 # Run http-server and save its PID
