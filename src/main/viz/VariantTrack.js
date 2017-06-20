c26dba15bbd7db754d6b2848959c495493077ac2
Gene Selkov <gene.selkov@tempus.com>
2017-06-20 15:17:04 -0500

    worked out the data structures for showing a VCF blacklist popup

--- src/main/viz/VariantTrack.js
+++ src/main/viz/VariantTrack.js
@@ -78,6 +78,7 @@ class VariantTrack extends React.Component {
 
     ctx.fillStyle = style.VARIANT_FILL;
     ctx.strokeStyle = style.VARIANT_STROKE;
+
     variants.forEach(variant => {
       ctx.pushObject(variant);
       var x = Math.round(scale(variant.position));
@@ -166,98 +167,125 @@ class BlacklistTrack extends VariantTrack {
     // ctx.fillStyle = style.VARIANT_FILL;
     ctx.strokeStyle = style.VARIANT_STROKE;
     ctx.font = style.TIGHT_TEXT_STYLE;
+
+    // Scan VCF loci to see whether they have variants on both strands or just one.
+    // Also, transform the list of variants, groupping them by position.
+    var strand = {};
+    var locus_var = {};
     variants.forEach(variant => {
-      ctx.pushObject(variant);
-      var x = Math.round(scale(variant.position));
-      var width = Math.round(scale(variant.position + 1)) - 1 - x;
-      var type, bstrand, symbol;
-      var vcfdata = variant.vcfLine.split('\t')[7];
-      if (vcfdata.match(/OID=/)) { // Ion Torrent black list
-        variant.vcfLine.split('\t')[7].split(';').forEach(chunk => {
-          let [key, value] = chunk.split('=');
-          if (key === 'OID') {
-            type = value.split('.')[0];
-          }
-          if (key === 'BSTRAND') {
-            bstrand = value;
-          }
-        });
-        switch (bstrand) {
-          case 'F':
-            ctx.fillStyle = style.ALIGNMENT_PLUS_STRAND_COLOR;
-          break;
-          case 'R':
-            ctx.fillStyle = style.ALIGNMENT_MINUS_STRAND_COLOR;
-          break;
-          default:
-            ctx.fillStyle = 'white';
-        }
-        switch (type) {
-          case 'SSE_SNP':
-            symbol = 'S';
-          break;
-          case 'SSE_DEL':
-            symbol = 'D';
-          break;
-          case 'SSE_INS':
-            symbol = 'I';
-          break;
-          case 'AMPL_LEFT':
-            symbol = 'L';
-          break;
-          case 'AMPL_RIGHT':
-            symbol = 'R';
-          break;
-          case 'LHP':
-            symbol = 'H';
-        }
-        ctx.fillRect(x - 0.5, y - 0.5, width, style.VARIANT_HEIGHT);
-        ctx.strokeRect(x - 0.5, y - 0.5, width, style.VARIANT_HEIGHT);
-        ctx.fillStyle = style.BLACKLIST_TEXT_COLOR;
-        ctx.fillText(symbol, x + 4, y + style.VARIANT_HEIGHT / 2 + 3);
+      var locus = `${variant.contig}:${variant.position}`;
+      if (!strand[locus]) {
+        strand[locus] = {};
       }
-      else {
-        // Presumbaly, it is a Tempus black list
-        var vcf = {strand: variant.vcfLine.split('\t')[6]};
-        vcfdata.split(';').forEach(chunk => {
-          let [key, value] = chunk.split('=');
-          vcf[key] = value;
-        });
-        ctx.globalAlpha = 0.65;
-        switch (vcf.strand) {
-          case 'F':
-            ctx.fillStyle = style.ALIGNMENT_PLUS_STRAND_COLOR;
-            ctx.fillRect(x - 0.5, y - 0.5, width, style.VARIANT_HEIGHT);
-          break;
-          case 'R':
-            ctx.fillStyle = style.ALIGNMENT_MINUS_STRAND_COLOR;
-            ctx.fillRect(x - 0.5, y - 0.5, width, style.VARIANT_HEIGHT);
-          break;
-          default:
-            ctx.fillStyle = 'white';
-        }
-        ctx.strokeRect(x - 0.5, y - 0.5, width, style.VARIANT_HEIGHT);
-        ctx.globalAlpha = 1.0;
+      strand[locus][variant.filter] = true;
+
+      if (!locus_var[locus]) {
+        locus_var[locus] = [];
       }
+      locus_var[locus].push(variant);
+    });
+
+    Object.keys(locus_var).forEach(locus => {
+      ctx.pushObject(locus_var[locus]);
+      locus_var[locus].forEach(variant => {
+        var locus = `${variant.contig}:${variant.position}`;
+        var x = Math.round(scale(variant.position));
+        var width = Math.round(scale(variant.position + 1)) - 1 - x;
+        var type, bstrand, symbol;
+        var vcfdata = variant.vcfLine.split('\t')[7];
+        if (vcfdata.match(/OID=/)) { // Ion Torrent black list
+          variant.vcfLine.split('\t')[7].split(';').forEach(chunk => {
+            let [key, value] = chunk.split('=');
+            if (key === 'OID') {
+              type = value.split('.')[0];
+            }
+            if (key === 'BSTRAND') {
+              bstrand = value;
+            }
+          });
+          switch (bstrand) {
+            case 'F':
+              ctx.fillStyle = style.ALIGNMENT_PLUS_STRAND_COLOR;
+            break;
+            case 'R':
+              ctx.fillStyle = style.ALIGNMENT_MINUS_STRAND_COLOR;
+            break;
+            default:
+              ctx.fillStyle = 'white';
+          }
+          switch (type) {
+            case 'SSE_SNP':
+              symbol = 'S';
+            break;
+            case 'SSE_DEL':
+              symbol = 'D';
+            break;
+            case 'SSE_INS':
+              symbol = 'I';
+            break;
+            case 'AMPL_LEFT':
+              symbol = 'L';
+            break;
+            case 'AMPL_RIGHT':
+              symbol = 'R';
+            break;
+            case 'LHP':
+              symbol = 'H';
+          }
+          ctx.fillRect(x - 0.5, y - 0.5, width, style.VARIANT_HEIGHT);
+          ctx.strokeRect(x - 0.5, y - 0.5, width, style.VARIANT_HEIGHT);
+          ctx.fillStyle = style.BLACKLIST_TEXT_COLOR;
+          ctx.fillText(symbol, x + 4, y + style.VARIANT_HEIGHT / 2 + 3);
+        }
+        else {
+          // Presumbaly, it is a Tempus black list
+          variant.strand = variant.filter;
+          if (strand[locus].F && strand[locus].R) {
+            // ctx.fillStyle = style.ALIGNMENT_PLUS_STRAND_COLOR;
+            ctx.fillStyle = '#ff9999';
+            ctx.beginPath();
+            ctx.moveTo(x - 0.5, y - 0.5);
+            ctx.lineTo(x - 0.5 + width, y - 0.5);
+            ctx.lineTo(x - 0.5, y - 0.5 + style.VARIANT_HEIGHT);
+            ctx.fill();
+
+            // ctx.fillStyle = style.ALIGNMENT_MINUS_STRAND_COLOR;
+            ctx.fillStyle = '#3399ff';
+            ctx.beginPath();
+            ctx.moveTo(x - 0.5 + width, y - 0.5 + style.VARIANT_HEIGHT);
+            ctx.lineTo(x - 0.5 + width, y - 0.5);
+            ctx.lineTo(x - 0.5, y - 0.5 + style.VARIANT_HEIGHT);
+            ctx.fill();
+
+/*
+          ctx.beginPath();
+          ctx.moveTo(x - 0.5 + width, y - 0.5);
+          ctx.lineTo(x - 0.5, y - 0.5 + style.VARIANT_HEIGHT);
+          ctx.stroke();
+*/
+          }
+          else {
+            switch (variant.strand) {
+              case 'F':
+                // ctx.fillStyle = style.ALIGNMENT_PLUS_STRAND_COLOR;
+                ctx.fillStyle = '#ff9999';
+              break;
+              case 'R':
+                // ctx.fillStyle = style.ALIGNMENT_MINUS_STRAND_COLOR;
+                ctx.fillStyle = '#3399ff';
+              break;
+              default:
+                ctx.fillStyle = 'white';
+            }
+            ctx.fillRect(x - 0.5, y - 0.5, width, style.VARIANT_HEIGHT);
+          }
+          ctx.strokeRect(x - 0.5, y - 0.5, width, style.VARIANT_HEIGHT);
+        }
+      });
       ctx.popObject();
     });
 
     ctx.restore();
-  }
-
-  handleClick(reactEvent: any) {
-    var ev = reactEvent.nativeEvent,
-        x = ev.offsetX,
-        y = ev.offsetY,
-        canvas = this.refs.canvas,
-        ctx = canvasUtils.getContext(canvas),
-        trackingCtx = new dataCanvas.ClickTrackingContext(ctx, x, y);
-    this.renderScene(trackingCtx);
-    var variant = trackingCtx.hit && trackingCtx.hit[0];
-    var alert = window.alert || console.log;
-    if (variant) {
-      alert(JSON.stringify(variant));
-    }
   }
 
   handleMouseMove(reactEvent: any) {
@@ -269,9 +297,9 @@ class BlacklistTrack extends VariantTrack {
         trackingCtx = new dataCanvas.ClickTrackingContext(ctx, x, y);
 
     this.renderScene(trackingCtx);
-    var variant = trackingCtx.hit && trackingCtx.hit[0];
-    if (variant) {
-      console.log(variant);
+    var bl = trackingCtx.hit && trackingCtx.hit[0];
+    if (bl) {
+      console.log(bl);
       this.refs.popup.open();
       this.refs.popup.domNode.style.left = reactEvent.pageX;
       this.refs.popup.domNode.style.top = reactEvent.pageY;
